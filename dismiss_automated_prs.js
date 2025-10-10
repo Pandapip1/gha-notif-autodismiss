86e3b9a666c4ed05f90f4d9cc84236da21cefffc
Gavin John <gavinnjohn@gmail.com>
2025-10-10 12:53:52 -0700

    Add throttling config

--- dismiss_automated_prs.js
+++ dismiss_automated_prs.js
@@ -8,7 +8,28 @@ if (!TOKEN) {
 }
 
 const ThrottledOctokit = Octokit.plugin(throttling);
-const octokit = new ThrottledOctokit({ auth: TOKEN });
+const octokit = new ThrottledOctokit({
+  auth: TOKEN,
+  throttle: {
+    onRateLimit: (retryAfter, options, octokit, retryCount) => {
+      octokit.log.warn(
+        `Request quota exhausted for request ${options.method} ${options.url}`,
+      );
+
+      if (retryCount < 1) {
+        // only retries once
+        octokit.log.info(`Retrying after ${retryAfter} seconds!`);
+        return true;
+      }
+    },
+    onSecondaryRateLimit: (retryAfter, options, octokit) => {
+      // does not retry, only logs a warning
+      octokit.log.warn(
+        `SecondaryRateLimit detected for request ${options.method} ${options.url}`,
+      );
+    },
+  },
+});
 
 // Config from environment
 const TARGET_OWNER = process.env.TARGET_OWNER;
