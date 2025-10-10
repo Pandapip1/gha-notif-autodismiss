ff6854f8cef1506aca1f1d64f07b7cac2d5054ba
Gavin John <gavinnjohn@gmail.com>
2025-10-10 12:50:44 -0700

    Switch to import

--- dismiss_automated_prs.js
+++ dismiss_automated_prs.js
@@ -1,4 +1,4 @@
-const { Octokit } = require("@octokit/rest");
+import { Octokit } from "@octokit/rest";
 import { throttling } from "@octokit/plugin-throttling";
 
 const TOKEN = process.env.GITHUB_TOKEN;
